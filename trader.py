15f54301e9783646205737b8305034758c808a81
Lei <45155667+leifu1128@users.noreply.github.com>
2023-03-21 10:39:30 -0400

    Data

--- trader.py
+++ trader.py
@@ -72,6 +72,86 @@ class BolBStrategy(BaseStrategy):
             self.orders[self.product] = [Order(self.product, self.data['top_ask'], 1)]
 
 
+class NaiveMM(BaseStrategy):
+    def __init__(self):
+        super().__init__()
+        self.product = "PEARLS"
+        self.pearls_limit = 20
+
+    def strategy(self) -> Dict[str, List[Order]]:
+        """
+        Takes all buy and sell orders for all symbols as an input,
+        and outputs a list of orders to be sent
+        """
+        print(vars(self.state))
+
+        for product in self.state.order_depths.keys():
+            if product == "PEARLS":
+                # Mean reversion strategy
+                bids: Dict[int, int] = self.state.order_depths[product].buy_orders
+                asks: Dict[int, int] = self.state.order_depths[product].sell_orders
+                print("bids: ", bids)
+                print("asks: ", asks)
+                position: int = self.state.position["PEARLS"] \
+                    if "PEARLS" in self.state.position.keys() else 0
+                orders: list[Order] = []
+
+                # Get best bid, best ask, and mid price
+                # NOTE: The if statements may not be necessary, not sure if
+                #       this challenge involves no-bid or no-ask situations?
+                if bids:
+                    best_bid = max(bids.keys())
+                    best_bid_volume = bids[best_bid]
+                if asks:
+                    best_ask = min(asks.keys())
+                    best_ask_volume = asks[best_ask]
+
+                # TODO: The following two if statements don't yet handle the situation
+                #           (best ask volume) < (remainder of position limit).
+                #       So they won't fill the "second-best bid/ask".
+                #       Make it do that.
+                # NOTE: positive quantity => buy order
+
+                # Fill asks below price
+                if position < self.pearls_limit and best_ask < 10000:
+                    qty = max(position - self.pearls_limit, best_ask_volume)  # NEGATIVE
+                    print("best_ask_volume: ", best_ask_volume)
+                    print("BUY", str(-qty), "at", best_ask)
+                    orders.append(Order(product, best_ask, -qty))
+
+                    if position + abs(qty) > 0:
+                        # We will be long pearls after this purchase.
+                        # Ask just above fair value.
+                        orders.append(Order(product, 10000, -(position + abs(qty))))
+                        print("ASK", str(-(position + abs(qty))), "at", 10000)
+                    elif position + abs(qty) < 0:
+                        # We will be short pearls after this purchase.
+                        # Bid just below fair value.
+                        orders.append(Order(product, 10000, -(position + abs(qty))))
+                        print("BID", str(-(position + abs(qty))), "at", 10000)
+
+                # Fill bids above price
+                if position > -self.pearls_limit and best_bid > 10000:
+                    qty = min(position + self.pearls_limit, best_bid_volume)  # POSITIVE
+                    print("best_bid_volume: ", best_bid_volume)
+                    print("SELL", str(qty), "at", best_bid)
+                    orders.append(Order(product, best_bid, -qty))
+
+                    if position - abs(qty) > 0:
+                        # We will be long pearls after this sale.
+                        # Ask just above fair value.
+                        orders.append(Order(product, 10000, -(position - abs(qty))))
+                        print("ASK", str(-(position - abs(qty))), "at", 10000)
+                    elif position - abs(qty) < 0:
+                        # We will be short pearls after this sale.
+                        # Bid just below fair value.
+                        orders.append(Order(product, 10000, -(position - abs(qty))))
+                        print("BID", str(-(position - abs(qty))), "at", 10000)
+
+                # Add orders to result dict
+                self.orders[product] = orders
+
+
 class BaseTrader:
     def __init__(self, strategies: Dict[str, BaseStrategy]):
         self.strategies = strategies
@@ -91,107 +171,4 @@ class BaseTrader:
 
 class Trader(BaseTrader):
     def __init__(self):
-        super().__init__({
-            'PEARLS': BolBStrategy(),
-        })
-
-
-'''class Trader:
-    pearls_ema = None
-    pearls_limit = 20
-
-    def run(self, state: TradingState) -> Dict[str, List[Order]]:
-        """
-        Takes all buy and sell orders for all symbols as an input,
-        and outputs a list of orders to be sent
-        """
-        result = {}
-
-        if self.pearls_ema:
-            print(str(round(self.pearls_ema, 2)).rjust(8, " "), state.position)
-
-        for product in state.order_depths.keys():
-            if product == "PEARLS":
-                # Mean reversion strategy
-                bids: Dict[int, int] = state.order_depths[product].buy_orders
-                asks: Dict[int, int] = state.order_depths[product].sell_orders
-                print("bids: ", bids)
-                print("asks: ", asks)
-                position: int = state.position["PEARLS"] \
-                    if "PEARLS" in state.position.keys() else 0
-                orders: list[Order] = []
-
-                # Get best bid, best ask, and mid price
-                # NOTE: The if statements may not be necessary, not sure if
-                #       this challenge involves no-bid or no-ask situations?
-                if bids:
-                    best_bid = max(bids.keys())
-                    best_bid_volume = bids[best_bid]
-                if asks:
-                    best_ask = min(asks.keys())
-                    best_ask_volume = asks[best_ask]
-                mid_price = (best_bid + best_ask) / 2
-
-                # Update EMA20
-                N = 20
-                k = 2 / (N + 1)
-                self.pearls_ema = mid_price * k + self.pearls_ema * (1 - k) \
-                    if self.pearls_ema else mid_price
-
-                # TODO: The following two if statements don't yet handle the situation
-                #           (best ask volume) < (remainder of position limit).
-                #       So they won't fill the "second-best bid/ask", even below EMA20.
-                #       Make it do that.
-                # NOTE: positive quantity => buy order
-
-                # Fill asks below EMA20
-                if position < self.pearls_limit and best_ask < self.pearls_ema:
-                    qty = max(position - self.pearls_limit,
-                              best_ask_volume)  # NEGATIVE
-                    print("best_ask_volume: ", best_ask_volume)
-                    print("BUY", str(-qty), "at", best_ask)
-                    orders.append(Order(product, best_ask, -qty))
-
-                    if position + abs(qty) > 0:
-                        # We will be long pearls after this purchase.
-                        # Ask just above fair value.
-                        orders.append(
-                            Order(product, ceil(self.pearls_ema), -(position + abs(qty))))
-                        print("ASK", str(-(position + abs(qty))),
-                              "at", ceil(self.pearls_ema))
-                    elif position + abs(qty) < 0:
-                        # We will be short pearls after this purchase.
-                        # Bid just below fair value.
-                        orders.append(
-                            Order(product, floor(self.pearls_ema), -(position + abs(qty))))
-                        print("BID", str(-(position + abs(qty))),
-                              "at", floor(self.pearls_ema))
-
-                # Fill bids above EMA20
-                if position > -self.pearls_limit and best_bid > self.pearls_ema:
-                    qty = min(position + self.pearls_limit,
-                              best_bid_volume)  # POSITIVE
-                    print("best_bid_volume: ", best_bid_volume)
-                    print("SELL", str(qty), "at", best_bid)
-                    orders.append(Order(product, best_bid, -qty))
-
-                    if position - abs(qty) > 0:
-                        # We will be long pearls after this sale.
-                        # Ask just above fair value.
-                        orders.append(
-                            Order(product, ceil(self.pearls_ema), -(position - abs(qty))))
-                        print("ASK", str(-(position - abs(qty))),
-                              "at", ceil(self.pearls_ema))
-                    elif position - abs(qty) < 0:
-                        # We will be short pearls after this sale.
-                        # Bid just below fair value.
-                        orders.append(
-                            Order(product, floor(self.pearls_ema), -(position - abs(qty))))
-                        print("BID", str(-(position - abs(qty))),
-                              "at", floor(self.pearls_ema))
-
-                # Add orders to result dict
-                result[product] = orders
-
-        print()
-        return result'''
+        super().__init__({'PEARLS': NaiveMM()})
