109f4bcd8a8104f430b20051dbe93979a7ecabc5
Lei <45155667+leifu1128@users.noreply.github.com>
2023-03-23 17:22:47 -0400

    Update

--- utils.py
+++ utils.py
@@ -2,5 +2,5 @@ import pandas as pd
 
 
 def load_product(obj, product):
-    df = pd.read_csv(f'round2/ob_{obj}.csv', delimiter=';')
+    df = pd.read_csv(f'round2/ob_{obj}.csv', delimiter=';', index_col=0)
     return df.loc[df['product'] == product]
